081bdf3542095ac220a56248209567538ba23464
Evan Rushton <evan.rushton@gmail.com>
2018-09-17 16:33:36 -0700

    Ran correlations with non-tutorial core levels - still low

--- B. Game : Survey Comparison.R
+++ B. Game : Survey Comparison.R
@@ -8,8 +8,8 @@ library(reshape2)
 ans <- read.table("./Data/submit_answer.csv", fill = TRUE, header = TRUE, sep = ",")
 
 # Select 1st attempts only
-ans <- ans[which(ans$attempt_count == 1), ]
-ans <- ans[ order(ans[,2], ans[,5]), ] # 8524 rows
+ans <- ans[which(ans$attempt_count == 1), ] # 8524 rows
+ans <- ans[ order(ans[,2], ans[,5]), ] # Order by userId and gameLevel
 # Find duplicate uid/game level
 ans <- ans[!duplicated(ans[c("userId","gameLevel")]), ] # Remove duplicates (8407 rows)
 
@@ -23,21 +23,23 @@ level_labels <- level_labels[ order(level_labels[,1]), ]
 all_levels <- as.character(level_labels$gameLevel)
 # Remove hand-feeding challenges T2.01 T1.01 T2.01.REPLACE (adult ram, baby ram, 3 baby ram)
 all_levels <- setdiff(all_levels, c("T2.01", "T1.01", "T2.01-REPLACE"))
-core_levels <- c("T1.02a", "T1.03", "T1.05", "T1.04", "T1.07a", "1.05b", "1.09a", "1.08", "1.07a", "1.06", "2.01c", "2.06b", "2.05a", "2.04b", "2.03a", "T3.01a", "3.01b", "3.03b", "3.03d", "T4.01a", "T4.01b", "T4.02", "T4.03a", "4.07b", "4.1", "4.16", "4.17", "4.13")
-review_levels <- c("1.01a", "1.01b", "1.02b", "1.02c", "1.03a", "1.03c", "1.04a", "1.05a", "1.05c", "1.07b", "2.01a", "2.01b", "2.02a", "2.02c", "2.03b", "2.04a", "2.06a", "3.01a", "3.01c", "3.03a", "4.02a", "4.03a", "4.03b", "4.03c", "4.04a", "4.07b", "4.08", "4.11", "4.15")
-non_tutorial_core <- c("1.05b", "1.09a", "1.08", "1.07a", "1.06", "2.01c", "2.06b", "2.05a", "2.04b", "2.03a", "3.01b", "3.03b", "3.03d", "4.07b", "4.1", "4.16", "4.17", "4.13")
-tutorial_core <- c("T1.02a", "T1.03", "T1.05", "T1.04", "T1.07a", "T3.01a", "T4.01a", "T4.01b", "T4.02", "T4.03a")
-core1_levels <- c("T1.02a", "T1.03", "T1.05", "T1.04", "T1.07a", "1.05b", "1.09a", "1.08", "1.07a", "1.06")
-review1_levels <- c("1.01a", "1.01b", "1.02b", "1.02c", "1.03a", "1.03c", "1.04a", "1.05a", "1.05c", "1.07b")
-core2_levels <- c("2.01c", "2.06b", "2.05a", "2.04b", "2.03a")
-review2_levels <- c("2.01a", "2.01b", "2.02a", "2.02c", "2.03b", "2.04a", "2.06a")
-core3_levels <- c("T3.01a", "3.01b", "3.03b", "3.03d")
-review3_levels <- c("3.01a", "3.01c", "3.03a")
-core4_levels <- c("T4.01a", "T4.01b", "T4.02", "T4.03a", "4.07b", "4.1", "4.16", "4.17", "4.13")
-review4_levels <- c("4.02a", "4.03a", "4.03b", "4.03c", "4.04a", "4.07b", "4.08", "4.11", "4.15")
-
-
-
+core_levels <- c("T1.02a", "T1.03", "T1.05", "T1.04", "T1.07a", "1.05b", "1.09a", "1.08", "1.07a", "1.06", "2.01c", "2.06b", "2.05a", "2.04b", "2.03a", "T3.01a", "3.01b", "3.03b", "3.03d", "T4.01a", "T4.01b", "T4.02", "T4.03a", "4.07b", "4.1", "4.16", "4.17", "4.13") #28 levels
+review_levels <- c("1.01a", "1.01b", "1.02b", "1.02c", "1.03a", "1.03c", "1.04a", "1.05a", "1.05c", "1.07b", "2.01a", "2.01b", "2.02a", "2.02c", "2.03b", "2.04a", "2.06a", "3.01a", "3.01c", "3.03a", "4.02a", "4.03a", "4.03b", "4.03c", "4.04a", "4.07b", "4.08", "4.11", "4.15") #29 levels
+non_tutorial_core <- c("1.05b", "1.09a", "1.08", "1.07a", "1.06", "2.01c", "2.06b", "2.05a", "2.04b", "2.03a", "3.01b", "3.03b", "3.03d", "4.07b", "4.1", "4.16", "4.17", "4.13") #18 levels
+tutorial_core <- c("T1.02a", "T1.03", "T1.05", "T1.04", "T1.07a", "T3.01a", "T4.01a", "T4.01b", "T4.02", "T4.03a") #10 levels
+core1_levels <- c("T1.02a", "T1.03", "T1.05", "T1.04", "T1.07a", "1.05b", "1.09a", "1.08", "1.07a", "1.06") #10 levels
+review1_levels <- c("1.01a", "1.01b", "1.02b", "1.02c", "1.03a", "1.03c", "1.04a", "1.05a", "1.05c", "1.07b") #10 levels
+ntc1_levels <- c("1.05b", "1.09a", "1.08", "1.07a", "1.06") #5 levels
+core2_levels <- c("2.01c", "2.06b", "2.05a", "2.04b", "2.03a") #5 levels
+review2_levels <- c("2.01a", "2.01b", "2.02a", "2.02c", "2.03b", "2.04a", "2.06a") #7 levels
+ntc2_levels <- c("2.01c", "2.06b", "2.05a", "2.04b", "2.03a") #5 levels
+core3_levels <- c("T3.01a", "3.01b", "3.03b", "3.03d") #4 levels
+review3_levels <- c("3.01a", "3.01c", "3.03a") #3 levels
+ntc3_levels <- c("3.01b", "3.03b", "3.03d") #3 levels
+core4_levels <- c("T4.01a", "T4.01b", "T4.02", "T4.03a", "4.07b", "4.1", "4.16", "4.17", "4.13") #9 levels
+review4_levels <- c("4.02a", "4.03a", "4.03b", "4.03c", "4.04a", "4.07b", "4.08", "4.11", "4.15") #9 levels
+ntc4_levels <- c("4.07b", "4.1", "4.16", "4.17", "4.13") #5 levels
+  
 # Pivot answer submissions to make response vectors
 resp <- ans[, c(2,5,9)]
 resp <- dcast(resp, userId ~ gameLevel, fill = 3, fun.aggregate = mean)
@@ -62,32 +64,39 @@ resp %>% select(which(names(resp) %in% tutorial_core)) %>% rowSums(na.rm=TRUE) -
 resp %>% select(which(names(resp) %in% c(review1_levels, core1_levels))) %>% rowSums(na.rm=TRUE) -> resp$L1Tot
 resp %>% select(which(names(resp) %in% core1_levels)) %>% rowSums(na.rm=TRUE) -> resp$core1Tot
 resp %>% select(which(names(resp) %in% review1_levels)) %>% rowSums(na.rm=TRUE) -> resp$review1Tot
+resp %>% select(which(names(resp) %in% ntc1_levels)) %>% rowSums(na.rm=TRUE) -> resp$ntc1Tot
 resp %>% select(which(names(resp) %in% c(review2_levels, core2_levels))) %>% rowSums(na.rm=TRUE) -> resp$L2Tot
 resp %>% select(which(names(resp) %in% core2_levels)) %>% rowSums(na.rm=TRUE) -> resp$core2Tot
 resp %>% select(which(names(resp) %in% review2_levels)) %>% rowSums(na.rm=TRUE) -> resp$review2Tot
+resp %>% select(which(names(resp) %in% ntc2_levels)) %>% rowSums(na.rm=TRUE) -> resp$ntc2Tot
 resp %>% select(which(names(resp) %in% c(review3_levels, core3_levels))) %>% rowSums(na.rm=TRUE) -> resp$L3Tot
 resp %>% select(which(names(resp) %in% core3_levels)) %>% rowSums(na.rm=TRUE) -> resp$core3Tot
 resp %>% select(which(names(resp) %in% review3_levels)) %>% rowSums(na.rm=TRUE) -> resp$review3Tot
+resp %>% select(which(names(resp) %in% ntc3_levels)) %>% rowSums(na.rm=TRUE) -> resp$ntc3Tot
 resp %>% select(which(names(resp) %in% c(review4_levels, core4_levels))) %>% rowSums(na.rm=TRUE) -> resp$L4Tot
 resp %>% select(which(names(resp) %in% core4_levels)) %>% rowSums(na.rm=TRUE) -> resp$core4Tot
 resp %>% select(which(names(resp) %in% review4_levels)) %>% rowSums(na.rm=TRUE) -> resp$review4Tot
+resp %>% select(which(names(resp) %in% ntc4_levels)) %>% rowSums(na.rm=TRUE) -> resp$ntc4Tot
 
 # Check level combinations after level 1 (respCombos) {2-3, 2-4, 3-4} 
 resp <- mutate(resp, L23Tot = L2Tot + L3Tot)
 resp <- mutate(resp, core23Tot = core2Tot + core3Tot)
 resp <- mutate(resp, review23Tot = review2Tot + review3Tot)
+resp <- mutate(resp, ntc23Tot = ntc2Tot + ntc3Tot)
 resp <- mutate(resp, L24Tot = L2Tot + L3Tot + L4Tot)
 resp <- mutate(resp, core24Tot = core2Tot + core3Tot + core4Tot)
 resp <- mutate(resp, review24Tot = review2Tot + review3Tot + review4Tot)
+resp <- mutate(resp, ntc24Tot = ntc2Tot + ntc3Tot + ntc4Tot)
 resp <- mutate(resp, L34Tot = L3Tot + L4Tot)
 resp <- mutate(resp, core34Tot = core3Tot + core4Tot)
 resp <- mutate(resp, review34Tot = review3Tot + review4Tot)
+resp <- mutate(resp, ntc34Tot = ntc3Tot + ntc4Tot)
 
 # Form matrices of different response vectors to compare correlations
 names(resp)[1] <- "uid"
 respTotals <- resp[, c("uid", "allTot", "coreTot", "reviewTot", "nonTutorialCoreTot", "tutorialCoreTot")]
-respLevels <- resp[, c("uid", "L1Tot", "core1Tot", "review1Tot", "L2Tot", "core2Tot","review2Tot", "L3Tot", "core3Tot", "review3Tot", "L4Tot", "core4Tot", "review4Tot")]
-respCombos <- resp[, c("uid", "L23Tot", "core23Tot", "review23Tot", "L24Tot", "core24Tot","review24Tot", "L34Tot", "core34Tot", "review34Tot")]
+respLevels <- resp[, c("uid", "L1Tot", "core1Tot", "review1Tot", "ntc1Tot", "L2Tot", "core2Tot","review2Tot", "ntc2Tot", "L3Tot", "core3Tot", "review3Tot", "ntc3Tot", "L4Tot", "core4Tot", "review4Tot", "ntc4Tot")]
+respCombos <- resp[, c("uid", "L23Tot", "core23Tot", "review23Tot", "ntc23Tot", "L24Tot", "core24Tot","review24Tot", "ntc24Tot", "L34Tot", "core34Tot", "review34Tot", "ntc34Tot")]
 
 # Load in pr/po scores and ability estimates (thetas)
 theta <- read.table("./Data/prpotheta.csv", fill = TRUE, header = TRUE, sep = ",") 
@@ -110,7 +119,11 @@ cor(com,com)[,c(1:5)]
 # Core levels 2-4 (core24Tot) have a positive correlation (0.37 po, 0.407 pr)
 # Survey Slopes (diff) have a negative correlation with pre and a positive correlation with post (-0.266 pr, 0.389 po)
 
-lm <- totals$po_est ~ totals$nonTutorialCoreTot
+model <- lm(combos$po_est ~ combos$L24Tot)
+anova(model)
+summary(model)
+plot(model)
+
 
 # Load in game response vectors from Seth's data cleaning
 resp_val <- read.table("./Data/gameRespVec.csv", fill = TRUE, header = TRUE, sep = ",")
@@ -122,16 +135,16 @@ cor(num_val,num_val) # pr/po data is positively correlated.
 # 0.1735607, slight positive correlation Grand.Total with po_est
 
 # Correlation between in-game performance and pr/po ability estimates 
-gameperf <- totals$allTot
+gameperf <- totals$nonTutorialCoreTot
 thetapo <- totals$po_est
 diff <- totals$diff
 core24 <- combos$core24Tot
-cor(core24,thetapo) # 0.1793418 slight positive correlation
+cor(gameperf,thetapo) # allTot: 0.1793418 | core24: 0.3731535 (slight positive correlation)
 
 # 1:1 Plot of Game Performance vs Post Ability Estimates
 plot(gameperf, thetapo,
      pch = 16,
-     xlab="Game Performance",
+     xlab="Non-tutorial Core Game Performance",
      ylab="Ability Post")
 abline(-4,2/5, col="blue", lwd=2)
 
