d7e76c65fc1212b4201d1fbe4bf51d87a41ec909
Evan Rushton <evan.rushton@gmail.com>
2018-09-26 01:34:57 -0700

    Reduce to 10 or more responses to get estimates

--- C. IRT Game Data.R
+++ C. IRT Game Data.R
@@ -12,22 +12,30 @@ ans <- ans[which(ans$attempt_count == 1), ] # 8524 rows
 ans <- ans[ order(ans[,2], ans[,5]), ] # Order by userId and gameLevel
 # Find duplicate uid/game level
 ans <- ans[!duplicated(ans[c("userId","gameLevel")]), ] # Remove duplicates (8407 rows)
+# Remove hand-feeding challenges T2.01 T1.01 T2.01.REPLACE (adult ram, baby ram, 3 baby ram)
+ans <- setdiff(ans, ans[which(ans$gameLevel %in% c("T2.01", "T1.01", "T2.01-REPLACE")), ]) # 7325 rows
 
 # Select desired vars
 resp <- ans[, c(2,5,8,9)]
 
 # Pivot answer submissions to make response vectors (concatenate problem_type to gameLevel)
-resp <- dcast(resp, userId ~ gameLevel + problem_type, fill = 3, fun.aggregate = mean, value.var = "success")
+resp <- dcast(resp, userId ~ gameLevel + problem_type, fill = 3, fun.aggregate = mean, value.var = "success") # 689
 # Remove quotes //"
 names(resp) <- lapply(names(resp), function(y) gsub('\\"', "", y))
 # Replace non-attempts with NA and keep 0 for first attempt fails
 resp <- data.frame(lapply(resp, function(y) gsub("^3$", NA, y)), check.names = FALSE)
-sapply(resp, function(y) sum(length(which(!is.na(y))))) # Attempt counts
-# Remove hand-feeding challenges T2.01 T1.01 T2.01.REPLACE (adult ram, baby ram, 3 baby ram)
-resp <- resp[ , setdiff(names(resp), c("T2.01_3UR_intro_adult_rammus", "T1.01_2UR_intro_baby_ram", "T2.01-REPLACE_3UR_intro_adult_rammus-REPLACE"))]
+sapply(resp, function(y) sum(length(which(!is.na(y))))) # Attempt counts (item)
+person_attempts <- sapply(1:nrow(resp), function(y) sum(length(which(!is.na(resp[y,2:ncol(resp)]))))) # Attempt counts (person)
+resp <- resp[which(person_attempts > 1) ,] # 658 59
 resp <- data.frame(lapply(resp, function(x) as.numeric(as.character(x))), check.names = FALSE)
 
 df <- resp[-c(1)]
+core_levels <- c("T1.02a_2UR1MC", "T1.03_2UR1MC" , "T1.05_2UR1MF", "T1.04_2UR1MC", "T1.07a_2US_intro_baby_unifox", "1.05b_2US1NC", "1.09a_2US2NF", "1.08_2US1NC", "1.07a_2US2MF", "1.06_2UR1MF", "2.01c_3UR1MC", "2.06b_3UR2MTb", "2.05a_3UR2MTa", "2.04b_3UR2MF", "2.03a_3UR2MF", "T3.01a_2BR_intro_baby_bird", "3.01b_2BR1MC", "3.03b_2BR2MF", "3.03d_2BR2MF", "T4.01a_3BR_intro_adult_bird", "T4.01b_3BR1MC" , "T4.02_3US_intro_adult_unifox", "T4.03a_3US1MC", "4.07b_3US1NC", "4.1_3BR3MF", "4.16_3US2NF", "4.17_3US2NTa", "4.13_3BR2MTb") #28 levels
+
+df <- df[which(names(df) %in% core_levels)] # 658  28
+person_attempts <- sapply(1:nrow(df), function(y) sum(length(which(!is.na(df[y,1:ncol(df)]))))) # Attempt counts (person)
+df <- df[which(person_attempts > 10) ,] # 230 28
+
 #Rasch
 res.rasch <- RM(df)
 #Person Parameters
